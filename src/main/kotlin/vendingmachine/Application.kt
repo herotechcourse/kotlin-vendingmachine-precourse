9babf61b0a50c1527332df68dbe0bcded951b11a
arash039 <shojaa.arash@gmail.com>
2025-05-12 17:34:13 +0200

    feat(Application): completed main class as the entry point to the program

--- src/main/kotlin/vendingmachine/Application.kt
+++ src/main/kotlin/vendingmachine/Application.kt
@@ -1,5 +1,35 @@
 package vendingmachine
 
 fun main() {
-    // TODO: Implement the program
+    val inputView = InputView()
+    val vendingMachine = VendingMachine()
+
+    while (true) {
+        try {
+            val inputMachineHolds = inputView.validateAmountMachineHolds(inputView.readAmountMachineHolds())
+            vendingMachine.initializeCoinInventory(inputMachineHolds)
+            //OutputView.printCoins(vendingMachine.availableCoins, "Coins in the vending machine:")
+            OutputView.primaryCoinPrint(vendingMachine.availableCoins)
+            val availableProducts = inputView.processProductsList(inputView.readProductList())
+            vendingMachine.initializeProductInventory(availableProducts)
+            val userMoney = inputView.validateUserMoney(inputView.readUserMoneyAmount())
+            vendingMachine.initializeUserMoney(userMoney)
+            break
+        } catch (e: IllegalArgumentException) {
+            println("${e.message}\nTry again\n" )
+        }
+    }
+    while (true) {
+        OutputView.printAmount(vendingMachine.userMoneyAmount, "Inserted amount")
+        try {
+            vendingMachine.purchaseProcessor(inputView.purchaseReader())
+            if (vendingMachine.checkEndingConditions()) {
+                break
+            }
+        } catch (e: IllegalArgumentException) {
+            println("${e.message}\nTry again\n" )
+        }
+
+    }
+
 }
