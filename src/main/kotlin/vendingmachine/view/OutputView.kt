20bc0bc729f0d337069ed12b3538060ac94bc066
HyeonminKim <61939519+kimpeter2018@users.noreply.github.com>
2025-05-12 16:33:54 +0200

    feat: change displaying coin logic to foreach loop

--- src/main/kotlin/vendingmachine/view/OutputView.kt
+++ src/main/kotlin/vendingmachine/view/OutputView.kt
@@ -1,6 +1,7 @@
 package vendingmachine.view
 
 import vendingmachine.Coin
+import vendingmachine.util.Change
 import vendingmachine.model.Product
 
 class OutputView {
@@ -13,14 +14,23 @@ class OutputView {
         println("Inserted amount: ${amount} KRW")
     }
 
-    fun displayCoinChanges(coins: MutableList<Coin>){
+    fun displayInitialCoins(coins: List<Change>){
         println("Coins in the vending machine:")
         
-        println("500 KRW - ${coins.count { it == Coin.COIN_500 }}")
-        println("100 KRW - ${coins.count { it == Coin.COIN_100 }}")
-        println("50 KRW - ${coins.count { it == Coin.COIN_50 }}")
-        println("10 KRW - ${coins.count { it == Coin.COIN_10 }}")
+        coins.forEach {
+            println("${it.getCoin().amount} KRW - ${it.getCount()}")
+        }
+     
     }
+
+    // fun displayCoinChanges(coins: List<Change>){
+    //     println("Change returned:")
+        
+    //     println("500 KRW - ${coins.count { it == Coin.COIN_500 }}")
+    //     println("100 KRW - ${coins.count { it == Coin.COIN_100 }}")
+    //     println("50 KRW - ${coins.count { it == Coin.COIN_50 }}")
+    //     println("10 KRW - ${coins.count { it == Coin.COIN_10 }}")
+    // }
 
     fun displayProducts(products: List<Product>) {
         println("Products: ")
