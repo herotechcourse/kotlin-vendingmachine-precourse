aa78ed0502f9ab6e4b5a137e9ee5c098024f2a50
Petra Bencze <petra.bncz@gmail.com>
2025-05-12 15:03:06 +0200

    feat(InputView): add read input functions

--- src/main/kotlin/vendingmachine/InputView.kt
+++ src/main/kotlin/vendingmachine/InputView.kt
@@ -1,4 +1,52 @@
 package vendingmachine
 
+import camp.nextstep.edu.missionutils.Console
+
 object InputView {
+    fun getInitialAmount(): Int {
+        println("Enter the amount the vending machine holds:")
+        val amount = Console.readLine().trim().toIntOrNull()
+            ?: throw IllegalArgumentException("[ERROR]: input has to be an integer")
+        return amount
+    }
+
+    fun getInventory(): List<List<String>> {
+        println("\nEnter product names, prices, and quantities:")
+        val input = Console.readLine().trim().split(";")
+        require(input.isNotEmpty()) { "[ERROR]: input cannot be empty" }
+        val inventory: MutableList<List<String>> = mutableListOf()
+        for (str in input) {
+            inventory.add(splitInventory(str))
+        }
+        return inventory.toList()
+    }
+
+    private fun splitInventory(string: String): List<String> {
+        require(string.isNotEmpty() && string.startsWith('[') && string.endsWith(']')) {
+            "[ERROR]: invalid input format"
+        }
+        string.trim('[', ']')
+        val list = string.split(",")
+        require(list.size == 3) { "[ERROR]: invalid input format" }
+        for (elem in list) {
+            require(elem.isNotEmpty()) { "[ERROR]: invalid input format" }
+        }
+        return list
+    }
+
+    fun getPurchaseAmount(): Int {
+        println("\nPlease enter the amount of money:")
+        val input = Console.readLine().trim().toIntOrNull()
+            ?: throw IllegalArgumentException("[ERROR]: input has to be an integer")
+        return input
+    }
+
+    fun getProductName(): String {
+        println("\nPlease enter the name of the product to purchase:")
+        val input = Console.readLine().trim()
+        for (char in input) {
+            require(char.isLetter()) { "[ERROR]: input can only contain letters" }
+        }
+        return input
+    }
 }
