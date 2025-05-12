3f15f017582e8785f6a4702e47be698aa280940f
Petra Bencze <petra.bncz@gmail.com>
2025-05-12 16:16:22 +0200

    refactor(Executor): extract create machine function

--- src/main/kotlin/vendingmachine/Executor.kt
+++ src/main/kotlin/vendingmachine/Executor.kt
@@ -2,19 +2,26 @@ package vendingmachine
 
 class Executor() {
     fun run() {
+        createMachine()
+        InputView.getInventory()
+        InputView.getPurchaseAmount()
+        InputView.getProductName()
+    }
 
+    private fun createMachine() {
         while (true) {
             try {
                 val amount = InputView.getInitialAmount()
                 val vendingMachine = VendingMachine(amount)
-                break
+                OutputView.displayCoins(vendingMachine.coins)
+                return
             } catch (e: IllegalArgumentException) {
                 println(e.message)
             }
         }
+    }
 
-        InputView.getInventory()
-        InputView.getPurchaseAmount()
-        InputView.getProductName()
+    companion object {
+        private const val MAX_RETRY = 5
     }
 }
