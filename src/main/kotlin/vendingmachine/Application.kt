b6b6bfc53999d04f810b0d9ad2325251c2c95f0c
sopa <sopa.sara@gmail.com>
2025-05-12 17:50:03 +0200

    feat(app): code flow

--- src/main/kotlin/vendingmachine/Application.kt
+++ src/main/kotlin/vendingmachine/Application.kt
@@ -1,5 +1,27 @@
 package vendingmachine
 
+import vendingmachine.views.InputView
+import vendingmachine.views.OutputView
+
 fun main() {
-    // TODO: Implement the program
+    val MachineAmount = InputView.readVendingMachineAmount()
+    val initialTable = CoinTable.generateTable(MachineAmount)
+    OutputView.printInitialCoins(initialTable)
+
+    val products = InputView.readProducts()
+    val purchaseAmount = InputView.readPurchaseAmount()
+    OutputView.printVendingMachineamount(purchaseAmount)
+
+    while (purchaseAmount >= cheapestproduct) {
+        val purchaseProduct = InputView.readPurchaseProduct()
+        OutputView.printNewPurchase(purchaseAmount)
+    }
+
+
+
+
+
+
+
+
 }
