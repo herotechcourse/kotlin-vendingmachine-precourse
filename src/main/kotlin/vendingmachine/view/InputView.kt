31b0360c9f7889b7154ed6a43a743253a24ad13e
David Gonzalez <davidegonzale@faba83c1-f762-4e15-8012-9506388aa9f5.fritz.box>
2025-05-12 17:46:18 +0200

    feat: update InputView

--- src/main/kotlin/vendingmachine/view/InputView.kt
+++ src/main/kotlin/vendingmachine/view/InputView.kt
@@ -1,6 +1,7 @@
 package vendingmachine.view
 
 import camp.nextstep.edu.missionutils.Console
+import vendingmachine.Product
 
 class InputView {
     fun inputAmount(): Int {
@@ -16,15 +17,44 @@ class InputView {
         return amount
     }
 
-    fun productInput(): List<String> {
-        println("Enter product names, prices, and quantities:")
+    fun productInput(): List<Product> {
+        println("\nEnter product names, prices, and quantities:")
         val input = Console.readLine()
-        val groupOfProducts = input.split(";").map { it.trim() }
-        for ( x in groupOfProducts) {
-            val itemsList = x.replace("[", "").replace("]", "").split(",").map { it.trim() }
+            .trim()
+            .split(";")
+        require(input.isNotEmpty()) {"ERROR product can't be empty"}
+        val parsed = input.map { product ->
+            if (product.startsWith("[") && product.endsWith("]")) {
+                product.trim('[').trim(']')
+            }
+            else {
+                throw IllegalArgumentException("ERROR syntax is: [productName, price, quantity].")
+            }
         }
+        val products = parsed.map { product ->
+            val productInfo = product.split(",")
+            val productName = productInfo[0]
+            val productPrice = productInfo[1]
+                .toIntOrNull()
+                ?: throw IllegalArgumentException("[ERROR] Invalid input.")
+            require(productPrice % 10 == 0) { "[ERROR] Invalid input." }
+            require(productPrice > 100) { "[ERROR] Invalid input." }
+            val productQuantity = productInfo[2]
+                .toIntOrNull()
+                ?: throw IllegalArgumentException("[ERROR] Invalid input.")
+            Product(productName, productPrice, productQuantity)
+        }
+        return products
+    }
 
-        return groupOfProducts
+    fun inputProduct(products: List<Product>): Product {
+        println("Please enter the name of the product to purchase:")
+        val userInput = Console.readLine().trim()
+        val productNames = products.map { it.name }
+        require(userInput in productNames) { "ERROR Invalid input." }
+        val targetProduct = products.filter { it.name == userInput }[0]
+        require(targetProduct.isProductAvailable()) { "ERROR The product is out of stock." }
+        return targetProduct
     }
 
 }
