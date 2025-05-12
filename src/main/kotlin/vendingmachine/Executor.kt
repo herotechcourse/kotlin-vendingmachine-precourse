048ee583f090802c0a93c1ec6204931ff64220b6
Petra Bencze <petra.bncz@gmail.com>
2025-05-12 16:58:07 +0200

    feat(Executor): new functionality with reading purhcase amount

--- src/main/kotlin/vendingmachine/Executor.kt
+++ src/main/kotlin/vendingmachine/Executor.kt
@@ -2,23 +2,50 @@ package vendingmachine
 
 class Executor() {
     fun run() {
-        createMachine()
-        InputView.getInventory()
-        InputView.getPurchaseAmount()
-        InputView.getProductName()
+        val machine = createMachine()
+        //readInventory()
+        readPurchaseAmount(machine)
+        purchase(machine)
     }
 
-    private fun createMachine() {
+    private fun createMachine(): VendingMachine {
         while (true) {
             try {
                 val amount = InputView.getInitialAmount()
                 val vendingMachine = VendingMachine(amount)
                 OutputView.displayCoins(vendingMachine.coins)
+                return vendingMachine
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+
+    private fun readInventory() {
+        while (true) {
+            try {
+                InputView.getInventory()
                 return
             } catch (e: IllegalArgumentException) {
                 println(e.message)
             }
         }
+    }
+
+    private fun readPurchaseAmount(machine: VendingMachine) {
+        while (true) {
+            try {
+                machine.initBudget(InputView.getPurchaseAmount())
+                return
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+
+    private fun purchase(machine: VendingMachine) {
+        OutputView.displayInsertedAmount(machine.budget)
+        InputView.getProductName()
     }
 
     companion object {
