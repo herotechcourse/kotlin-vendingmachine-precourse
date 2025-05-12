36ed00ac4d9529bcc6acef485a1b9fa51b578bba
Taekeundo <pleasepose@gmail.com>
2025-05-12 14:17:06 +0200

    feat(main): implement basic input and output

--- src/main/kotlin/vendingmachine/Application.kt
+++ src/main/kotlin/vendingmachine/Application.kt
@@ -1,5 +1,27 @@
 package vendingmachine
 
+import vendingmachine.view.InputView
+import vendingmachine.view.OutputView
+
 fun main() {
-    // TODO: Implement the program
+    OutputView.output1()
+    InputView.input1()
+
+    OutputView.output2()
+
+    OutputView.output3()
+
+    OutputView.output4()
+    InputView.input4()
+
+    OutputView.output5()
+    InputView.input5()
+
+    OutputView.output6()
+
+    OutputView.output7()
+    InputView.input7()
+
+    OutputView.output8()
+    OutputView.output9()
 }
