8c5cdb3e8e2fb94e29af6ecf67058f7102d54a45
Duc <nguyenminhducci@gmail.com>
2025-05-12 17:38:16 +0200

    feat: add output view

--- src/main/kotlin/vendingmachine/views/OutputView.kt
+++ src/main/kotlin/vendingmachine/views/OutputView.kt
@@ -1,4 +1,13 @@
 package vendingmachine.views
 
 class OutputView {
+	fun printBalance(balance: Int) {
+		println("Inserted amount: $balance KRW")
+	}
+
+	fun printChange(change: HashMap<Int, Int>) {
+		for (key in change.keys) {
+			println("$key KRW - ${change[key]}")
+		}
+	}
 }
