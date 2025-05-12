d762523512979773a5539bbf88d848b66cbb6dc6
HyeonminKim <61939519+kimpeter2018@users.noreply.github.com>
2025-05-12 14:14:39 +0200

    feat: add randomly selecting coin changes logic

--- src/main/kotlin/vendingmachine/controller/VendingMachineController.kt
+++ src/main/kotlin/vendingmachine/controller/VendingMachineController.kt
@@ -2,15 +2,34 @@ package vendingmachine.controller
 
 import vendingmachine.view.InputView
 import vendingmachine.view.OutputView
+import vendingmachine.Coin
+
+import camp.nextstep.edu.missionutils.Randoms
 
 class VendingMachineController {
     fun run() {
         val change = getInitialChange()
-        OutputView().displayInt(change)
+        val coinsInVendingMachine = getChangeCoins(change)
+
+        OutputView().displayCoinChanges(coinsInVendingMachine)
     }
 
     fun getInitialChange(): Int {
         val change = InputView.enterInitialChange()
         return change
+    }
+
+    fun getChangeCoins(change: Int): MutableList<Coin>{
+        val coins = mutableListOf<Coin>()
+        var remaining = change
+
+        while (remaining >= 10) {
+            val amount = Randoms.pickNumberInList(listOf(500, 100, 50, 10))
+            if (remaining >= amount) {
+                remaining -= amount
+                coins.add(Coin.from(amount))
+            }
+        }
+        return coins
     }
 }
--- src/main/kotlin/vendingmachine/view/OutputView.kt
+++ src/main/kotlin/vendingmachine/view/OutputView.kt
@@ -1,9 +1,20 @@
 package vendingmachine.view
 
+import vendingmachine.Coin
+
 class OutputView {
 
     fun displayInt(input: Int) {
         println("Entered Amount: ${input}")
+    }
+
+    fun displayCoinChanges(coins: MutableList<Coin>){
+        println("Coins in the vending machine:")
+        
+        println("500 KRW - ${coins.count { it == Coin.COIN_500 }}")
+        println("100 KRW - ${coins.count { it == Coin.COIN_100 }}")
+        println("50 KRW - ${coins.count { it == Coin.COIN_50 }}")
+        println("10 KRW - ${coins.count { it == Coin.COIN_10 }}")
     }
 
 }
