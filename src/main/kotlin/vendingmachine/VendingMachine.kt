a74c90197b71581a4593cbbc156c18c55bf8651c
Petra Bencze <petra.bncz@gmail.com>
2025-05-12 16:02:22 +0200

    feat(VendingMachine): new vending machine class
    - holds amount
    - generates coins based on the amount

---
+++ src/main/kotlin/vendingmachine/VendingMachine.kt
@@ -0,0 +1,38 @@
+package vendingmachine
+
+import camp.nextstep.edu.missionutils.Randoms
+
+class VendingMachine(private val _amount: Int) {
+    val amount: Int
+        get() = _amount
+
+    private val _coins: MutableMap<Coin, Int> = generateCoins()
+    val coins: Map<Coin, Int>
+        get() = _coins.toMap()
+
+    init {
+        require(amount % 10 == 0) { "[ERROR]: amount has to be divisible by 10" }
+        require(amount > 0) { "[ERROR]: amount has to be bigger than 0" }
+        // TODO add max amount
+    }
+
+    private fun generateCoins(): MutableMap<Coin, Int> {
+        val coins = mutableMapOf(Coin.COIN_500 to 0, Coin.COIN_100 to 0, Coin.COIN_50 to 0, Coin.COIN_10 to 0)
+        var remaining = _amount
+        while (remaining >= 10) {
+            val num = Randoms.pickNumberInList(listOf(500, 100, 50, 10))
+            if (remaining >= num) {
+                remaining -= num
+            }
+            // TODO extract to a sep fun
+            when(num) {
+                500 -> coins[Coin.COIN_500] = coins.getOrDefault(Coin.COIN_500, 0) + 1
+                100 -> coins[Coin.COIN_100] = coins.getOrDefault(Coin.COIN_100, 0) + 1
+                50 -> coins[Coin.COIN_50] = coins.getOrDefault(Coin.COIN_50, 0) + 1
+                10 -> coins[Coin.COIN_10] = coins.getOrDefault(Coin.COIN_10, 0) + 1
+            }
+        }
+        return coins
+    }
+
+}
