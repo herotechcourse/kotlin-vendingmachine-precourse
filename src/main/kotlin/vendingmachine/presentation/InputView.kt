5f08c46757db1666c8ebd75c93107703c10ebe7c
Farhana Ahmed Fima <farhana.ahmed.fima@gmail.com>
2025-05-12 16:10:57 +0200

    feat: implement InputView.kt to take user inputs

--- src/main/kotlin/vendingmachine/presentation/InputView.kt
+++ src/main/kotlin/vendingmachine/presentation/InputView.kt
@@ -1,5 +1,29 @@
 package vendingmachine.presentation
 
+import camp.nextstep.edu.missionutils.Console
+
 interface InputView {
+    fun readInitialAmount(): String
+    fun readListOfProducts(): String
+    fun readInsertedAmount(): String
+    fun readProductName(): String
+}
+
+class InputViewImpl : InputView{
+    override fun readInitialAmount(): String {
+        return Console.readLine()
+    }
+
+    override fun readListOfProducts(): String {
+        return Console.readLine()
+    }
+
+    override fun readInsertedAmount(): String {
+        return Console.readLine()
+    }
+
+    override fun readProductName(): String {
+        return Console.readLine()
+    }
 
 }
