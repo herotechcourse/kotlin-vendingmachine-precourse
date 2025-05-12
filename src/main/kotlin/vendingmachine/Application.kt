953069b33b0cc8e32212414dfc8a449490af65d0
tharisha <lekamgetharisha@gmail.com>
2025-05-12 16:40:15 +0200

    added showpurchase function

--- src/main/kotlin/vendingmachine/Application.kt
+++ src/main/kotlin/vendingmachine/Application.kt
@@ -1,5 +1,21 @@
 package vendingmachine
 
-fun main() {
-    // TODO: Implement the program
+fun main(){
+    val drinks = listOf(
+        Drink("Cola", 1500,20),
+        Drink("Soda", 1000, 10)
+    )
+
+    val vendingMachine = vendingMachine(drinks)
+    println("Welcome to vending machine")
+
+    fun showPurchase(drink: Drink?){
+      println("You bought a ${drink?.name}!")
+    }
+
+    fun showRemainingMoney(money:Int){
+        println("Remaining Money : ${money}")
+    }
+
 }
+
