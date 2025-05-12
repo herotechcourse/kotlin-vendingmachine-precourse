8f97c40e274feae4f2a87a839625a9ec18f3a7f2
Sanghun <po4865@gmail.com>
2025-05-12 15:52:51 +0200

    fix(OutputView): add print line for 'showHoldingCoins()'

--- src/main/kotlin/vendingmachine/OutputView.kt
+++ src/main/kotlin/vendingmachine/OutputView.kt
@@ -2,6 +2,7 @@ package vendingmachine
 
 object OutputView {
     fun showHoldingCoins(machine: Machine) {
+        println("\nCoins in the vending machine:")
         machine.coinBoard.forEach { coin, count ->
             if (coin != Coin.NONE) { println(machine.getCoinInfo(coin)) }
         }
