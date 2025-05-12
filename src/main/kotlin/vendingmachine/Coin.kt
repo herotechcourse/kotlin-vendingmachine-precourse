b45b6fa815f4ef4f024bc9c7057dfa36cf602589
arash039 <shojaa.arash@gmail.com>
2025-05-12 17:27:43 +0200

    feat(Coin): completed Coin enum class
    - Added two methods to find and return the coins based on certain inputs

--- src/main/kotlin/vendingmachine/Coin.kt
+++ src/main/kotlin/vendingmachine/Coin.kt
@@ -5,6 +5,24 @@ enum class Coin(val amount: Int) {
     COIN_100(100),
     COIN_50(50),
     COIN_10(10),
+    NONE(amount = 0);
 
-    // TODO: Implement additional functions
+    companion object {
+        fun coinFinder(amount: Int): Coin {
+            if (amount == 500) return COIN_500
+            if (amount == 100) return COIN_100
+            if (amount == 50) return COIN_50
+            if (amount == 10) return COIN_10
+            return NONE
+        }
+
+        fun printCoin(coin: Coin): String {
+            if (coin == COIN_500) return "500 KRW"
+            if (coin == COIN_100) return "100 KRW"
+            if (coin == COIN_50) return "50 KRW"
+            if (coin == COIN_10) return "10 KRW"
+            return ""
+        }
+
+    }
 }
