94ca7c2ae4d141db906b171d2ee14c0c81bbbd2e
Zaid <dagbanbia@gmail.com>
2025-05-12 17:58:53 +0200

    feat: display

--- src/main/kotlin/vendingmachine/OutputHandler.kt
+++ src/main/kotlin/vendingmachine/OutputHandler.kt
@@ -4,8 +4,10 @@ class OutputHandler{
 
     fun displayCoins(numscoins:List<Int>){
         println(numscoins)
-        for (i in numscoins){
+        println("500 KRW - ${numscoins[0]}")
+        println("100 KRW - ${numscoins[1]}")
+        println("50 KRW - ${numscoins[2]}")
+        println("10 KRW - ${numscoins[3]}")
 
         }
-    }
 }
