b5fbaa0f02a3e74bee69fab7b6ac9686e80e5732
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2025-05-12 14:22:27 +0200

    feat: Initial design for Product class

---
+++ src/main/kotlin/vendingmachine/Product.kt
@@ -0,0 +1,11 @@
+package vendingmachine
+
+class Product (private val name: String) {
+    private val price : Int = 0
+    private val quantity : Int = 0
+
+    private fun validatePrice()
+    private fun validateQuantity()
+    private fun validatePurchase()
+    private fun checkAvailability()
+}
