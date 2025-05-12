1f0227de246f0cac5879c6398dffd06f28a48656
Farhana Ahmed Fima <farhana.ahmed.fima@gmail.com>
2025-05-12 17:37:19 +0200

    feat: add processInitialAmount() to VendingMachineController

--- src/main/kotlin/vendingmachine/controller/VendingMachineController.kt
+++ src/main/kotlin/vendingmachine/controller/VendingMachineController.kt
@@ -1,10 +1,27 @@
 package vendingmachine.controller
 
+import vendingmachine.presentation.InputView
+import vendingmachine.presentation.OutputView
+
 interface VendingMachineController {
-    fun run ()
+    fun run()
 }
 
-class VendingMachineControllerImpl : VendingMachineController {
+class VendingMachineControllerImpl(
+    private val inputView: InputView,
+    private val outputView: OutputView,
+) : VendingMachineController {
+
+    private fun processInitialAmount(): Int {
+        outputView.printInputPrompt(INITIAL_AMOUNT_MESSAGE)
+        val input = inputView.readInitialAmount().toInt()
+        return input
+    }
+
     override fun run() {
+    }
+
+    companion object {
+        private const val INITIAL_AMOUNT_MESSAGE = "Enter the amount the vending machine holds:"
     }
 }
