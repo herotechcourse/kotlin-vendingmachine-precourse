3a22df39eb74bd90a29802de2150423e2eb6b21d
Adham Abdelfattah <50050146+adhamali74@users.noreply.github.com>
2025-05-12 17:52:02 +0200

    main app start

--- src/main/kotlin/vendingmachine/Application.kt
+++ src/main/kotlin/vendingmachine/Application.kt
@@ -1,5 +1,28 @@
 package vendingmachine
 
+import vendingmachine.domain.Wallet
+import vendingmachine.service.VendingMachine
+import vendingmachine.view.InputView
+import vendingmachine.view.OutputView
+
 fun main() {
-    // TODO: Implement the program
+    val machine = VendingMachine(mutableMapOf())
+
+    val machineAmount = InputView.readMachineCoinAmount()
+    machine.generateCoins(machineAmount)
+
+    OutputView.printMachineCoins(machine.getCoinStock())
+
+    val productInput = InputView.readProductInput()
+    machine.setProduct(productInput)
+
+    val inserted = InputView.readInsertedAmount()
+    val wallet = Wallet(inserted)
+
+    OutputView.printInsertedAmount(wallet.currentBalance())
+
+    machine.startSession(wallet)
+
+    val (changeMap, unreturnable) = machine.returnChange(wallet)
+    OutputView.printChange(changeMap, unreturnable)
 }
