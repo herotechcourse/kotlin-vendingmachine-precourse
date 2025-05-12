d4389f2c8f26d40194c339195af40fe9676ee109
Mariia Kulikova <111587633+mariyakulikova@users.noreply.github.com>
2025-05-12 15:39:14 +0200

    feat(VEnding Machine) override toString function in enum class Coin

--- src/main/kotlin/vendingmachine/Coin.kt
+++ src/main/kotlin/vendingmachine/Coin.kt
@@ -4,7 +4,9 @@ enum class Coin(val amount: Int) {
     COIN_500(500),
     COIN_100(100),
     COIN_50(50),
-    COIN_10(10),
+    COIN_10(10);
 
-    // TODO: Implement additional functions
+    override fun toString(): String {
+        return "${amount.toString()} KRW"
+    }
 }
