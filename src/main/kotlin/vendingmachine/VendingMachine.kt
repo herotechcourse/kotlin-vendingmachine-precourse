6134f11f9cbdc47758eb94304865da86496b0a76
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2025-05-12 17:28:46 +0200

    feat: created attributes cheapestProduct and userBudget in order to keep track of user's budget and be mindful when to break of runMachine() method. Implementation of runMachine method, responsible for the service of the program.

--- src/main/kotlin/vendingmachine/VendingMachine.kt
+++ src/main/kotlin/vendingmachine/VendingMachine.kt
@@ -2,6 +2,17 @@ package vendingmachine
 
 class VendingMachine (private val initAMount: Int) {
     private val inventory = mutableMapOf<Product, Int>()
+    private var cheapestProduct : Int = 0
+    private var userBudget : Int = 0
 
-    private fun runMachine()
+    fun runMachine() {
+        OutputView.displayCoins(initAMount)
+        InputView.handleInventory() //returns a list of products, maybe generate map later
+        cheapestProduct = inventory.minOf{it.value}
+        userBudget = InputView.handleUserBudget()
+        while (userBudget >= cheapestProduct) //incorrect, it needs to be bigger than the value of the cheapest product
+        {
+            val product = InputView.handlePurchaseOfProduct() //still not returning anything
+        }
+    }
 }
