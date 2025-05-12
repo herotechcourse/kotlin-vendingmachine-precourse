51a83105296dfc5820e830f09998c2f5107bfc15
Sanghun <po4865@gmail.com>
2025-05-12 14:07:57 +0200

    chore(main): add pseudocode to remind the progress of program

--- src/main/kotlin/vendingmachine/Application.kt
+++ src/main/kotlin/vendingmachine/Application.kt
@@ -1,5 +1,18 @@
 package vendingmachine
 
 fun main() {
-    // TODO: Implement the program
+    // take amount of money for machine to hold
+    // initialize machine with taken amount
+        // display generated holding coins in machine
+    // take product info, including name, price, qt
+    // take amount of money for user to use
+
+    // display remaining amount of money for user
+    // LOOP
+    // take name of product to purchase
+    // display remaining amount of money for user
+        // if must return, lack of remained amount of money
+        // display change to be returned
+            // if unable to return full amount
+            // display the amount of change to be returned but unable
 }
