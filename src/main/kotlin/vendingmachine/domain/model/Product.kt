5dd329a1ad0cf14793d79caed8c6867031dfd98a
HyeonminKim <61939519+kimpeter2018@users.noreply.github.com>
2025-05-12 14:57:01 +0200

    feat: add getters and setters for variables in Product class

--- src/main/kotlin/vendingmachine/domain/model/Product.kt
+++ src/main/kotlin/vendingmachine/domain/model/Product.kt
@@ -3,7 +3,15 @@ package vendingmachine.model
 class Product(
     private val name: String,
     private val cost: Int,
-    private val quantities: Int,
+    private var quantities: Int,
 ) {
+    fun getName(): String = name
+    
+    fun getCost(): Int = cost
 
+    fun getQuantities(): Int = quantities
+
+    fun setQuantities(newQunatities: Int) {
+        quantities = newQunatities
+    }
 }
