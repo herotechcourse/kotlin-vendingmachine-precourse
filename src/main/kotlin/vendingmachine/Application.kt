2e5f380d3d27fd690e418240da79bcd23d36d7e5
tharisha <lekamgetharisha@gmail.com>
2025-05-12 17:06:26 +0200

    Added new function and prinln status

--- src/main/kotlin/vendingmachine/Application.kt
+++ src/main/kotlin/vendingmachine/Application.kt
@@ -1,13 +1,22 @@
 package vendingmachine
+fun paresDetails(input: String): List<String>{
+    return input.split(",").map{it.trim()}
+}
+
+
 
 fun main(){
+    println("Welcome to vending machine")
+    println("Enter the amount the vending machine holds: ")
+    println("Enter product names, prices, and quantities: ")
     val drinks = listOf(
         Drink("Cola", 1500,20),
         Drink("Soda", 1000, 10)
     )
 
+    println("Please enter the amount of money: ")
     val vendingMachine = vendingMachine(drinks)
-    println("Welcome to vending machine")
+
 
     fun showPurchase(drink: Drink?){
       println("You bought a ${drink?.name}!")
