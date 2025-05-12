85e875c40e6c1cb158b26eac23b17488393e1aa6
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2025-05-12 15:49:40 +0200

    feat(service): add Product model and generate Products service

--- src/main/kotlin/vendingmachine/exception/VendingMachineException.kt
+++ src/main/kotlin/vendingmachine/exception/VendingMachineException.kt
@@ -1,7 +1,11 @@
 package vendingmachine.exception
 
-sealed class VendingMachineException(message: String): IllegalArgumentException("[ERROR] $message") {
-    class InvalidInputException(): VendingMachineException("Invalid Input.")
+sealed class VendingMachineException(message: String) : IllegalArgumentException("[ERROR] $message") {
+    class InvalidInputException() : VendingMachineException("Invalid Input.")
 
-    class InvalidCoinValueException(value: Int): VendingMachineException("Invalid Coin value: $value.")
+    class InvalidCoinValueException(value: Int) : VendingMachineException("Invalid Coin value: $value.")
+
+    class InvalidProductPriceException(name: String, value: Int) : VendingMachineException("Invalid Product Price: $name: $value.")
+
+    class InvalidProductQtyException(name: String, qty: Int) : VendingMachineException("Invalid Product Quantity: $name: $qty.")
 }
---
+++ src/main/kotlin/vendingmachine/model/VendingMachine.kt
@@ -0,0 +1,6 @@
+package vendingmachine.model
+
+import vendingmachine.Coin
+
+class VendingMachine(private val coins: List<Coin>, private val products: List<Product>) {
+}
